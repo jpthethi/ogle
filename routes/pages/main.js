af99a2d8b3abbebccd77ee4416bfa618dfdfdf2d
Jitendra <jitendra.thethi@aricent.com>
2018-02-01 23:50:04 +0530

    Html to pug and history page

--- routes/pages/main.js
+++ routes/pages/main.js
@@ -3,10 +3,33 @@ var router = express.Router();
 var index = require("../../index.js");
 var fs = require("fs");
 var moment = require("moment");
-
 var latest_file = "";
+
+var history = [];
+var current = {}
+
+fs.readFile('./history.json', (err, data) => {
+  if(err) console.log(err);
+  if(data!=undefined) history = JSON.parse(data);
+});
+
+
+router.get('/',function(req,res){
+  res.render("main/index")
+});
+
+router.get('/history',function(req,res){
+  res.render("main/history", {"history":history})
+});
+
 router.get('/fetch',function(req,res){
-  index.broadcast(JSON.stringify({"recognized":req.query.obj, "confidence": "100"}));
+  index.broadcast(JSON.stringify({"recognized":req.query.obj, "confidence": req.query.confidence}));
+  current.recognized = req.query.obj;
+  current.confidence = req.query.confidence;
+  current.recorded = new Date();
+  history.unshift(current);
+  current={}
+  flush_history();
   res.send(JSON.stringify({status:"ok"}))
 });
 
@@ -14,16 +37,23 @@ router.post('/upload',function(req,res){
   req.pipe(req.busboy);
   req.busboy.on('file', function (fieldname, file, filename) {
       latest_file = moment().format("DDMMYY_HHmmss") + ".jpg";
-      //console.log("Uploading: " + filename);
-      var fstream = fs.createWriteStream(__dirname + '/../..//public/files/' + latest_file);
+      if(current.file!=undefined) flush_history();
+      current={};
+      current.file = latest_file;
+      current.captured = new Date();
+      var fstream = fs.createWriteStream(__dirname + '/../../public/files/' + latest_file);
       file.pipe(fstream);
       fstream.on('close', function () {
-        console.log("send on socket_" + latest_file);
         index.broadcast(JSON.stringify({"file":latest_file}));
-        console.log("done - send on socket" + latest_file);
         res.send({status:"ok"})
       });
   });
 });
+
+function flush_history(){
+  fs.writeFile('./history.json', JSON.stringify(history), (err) => {
+    if(err) console.log(err);
+  });
+}
 
 module.exports = router;
